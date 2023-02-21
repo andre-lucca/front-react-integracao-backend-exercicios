501ca0a9573f629304467a141351b390a4ce5f3b
Andre Luca <lucaprojob@gmail.com>
2023-02-21 15:14:25 -0300

    Exerício finalizado

--- base-ex1/src/pages/Main/index.js
+++ base-ex1/src/pages/Main/index.js
@@ -1,9 +1,60 @@
+import { useEffect, useRef, useState } from 'react'
+import api from '../../api'
+
 import './styles.css';
 
 function Main() {
+  const [userList, setUserList] = useState([])
+  const nameInputRef = useRef(null)
+  const emailInputRef = useRef(null)
+
+  const getUsers = async () => {
+    const users = await api.get('/users')
+    setUserList(users.data)
+  }
+
+  const handleSetNewUser = async (event) => {
+    event.preventDefault()
+    const { value: name } = nameInputRef.current
+    const { value: email } = emailInputRef.current
+
+    await api.post('/users', {
+      name,
+      email,
+      password: '1234'
+    })
+
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="wrapper">
+      <form>
+        <input
+          ref={nameInputRef}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          ref={emailInputRef}
+          type="email"
+          placeholder="E-mail"
+        />
+        <button
+          type="submit"
+          onClick={handleSetNewUser}
+        >Cadastrar</button>
+      </form>
+      <ul className="user-container">
+        <h2>Users</h2>
+        {
+          userList.map(user => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
